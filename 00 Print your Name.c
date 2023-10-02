cafbf9ad00d0fde86e8fd6f257822f30c698fee9
Gaurav Jaiswal <142143367+Gkais31@users.noreply.github.com>
2023-10-02 18:02:51 +0530

    Add files via upload

---
+++ 00 Print your Name.c
@@ -0,0 +1,10 @@
+// C program to demonstrate printing of
+// our own name using printf()
+#include <stdio.h>
+
+int main()
+{
+	// print name
+	printf("Name : Gaurav Jaiswal");
+	return 0;
+}
---
+++ 09 Printing Integer.c
@@ -0,0 +1,14 @@
+// C Program to Print
+// Integer value
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+	// Declaring integer
+	int x = 5;
+
+	// Printing values
+	printf("Printing Integer value %d", x);
+	return 0;
+}
